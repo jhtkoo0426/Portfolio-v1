c6c9fbdc50b2cb130d60d3823818b7758c42963e
jhtkoo0426 <jhtkoo@gmail.com>
2022-04-30 20:40:58 +0100

    Added splash screen

--- src/App.js
+++ src/App.js
@@ -19,12 +19,11 @@ const App = () => {
   return isLoading ?
   	// If page is still loading then display the splash screen
   	<div className="splash-screen">
-      <h1 className="splash-screen-title">never gonna give you up</h1>
-      <h3>uwu</h3>
+      <h1 className="splash-screen-title">this is the splash screen...</h1>
     </div>:
   	<div className="app">
       <div className='intro'>
-        <h1 className='app-title'>Hewo, I'm Justin. I'm a Computer Science undergraduate based in London</h1>
+        <h1 className='app-title'>Hello, I'm Justin. I'm a Computer Science undergraduate based in London</h1>
         <h1 className='app-subtitle'>I'm interested in full-stack development with applied data science and machine learning.</h1>
       </div>
   	</div>
